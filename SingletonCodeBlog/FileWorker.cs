278a8eccace33d2fdfb8cc37fddc436262ff455c
Вадим Шванов <adm.shwan@gmail.com>
2019-06-18 17:32:04 +0300

    Добавьте файлы проекта.

---
+++ SingletonCodeBlog/FileWorker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace SingletonCodeBlog
+{
+    /// <summary>
+    /// Класс для работы с текстом. Сохранение изменений в файл 
+    /// выполняется только по запросу Save.
+    /// До этого изменения хранятся в динамической памяти.
+    /// </summary>
+    public class FileWorker
+    {
+        /// <summary>
+        /// Путь к файлу записи.
+        /// </summary>
+        public string FilePath { get; }
+
+        /// <summary>
+        /// Содержимое файла в динамической памяти.
+        /// </summary>
+        public string Text { get; private set; }
+
+        /// <summary>
+        /// Создать новый экземпляр для работы с текстом.
+        /// </summary>
+        public FileWorker()
+        {
+            FilePath = "test.txt";
+            ReadTextFromFile();
+        }
+
+        /// <summary>
+        /// Добавить текст в файл (без сохранения в постоянную память).
+        /// </summary>
+        /// <param name="text"> Добавляемый текст. </param>
+        public void WriteText(string text)
+        {
+            Text += text;
+        }
+
+        /// <summary>
+        /// Сохранить текст в файл.
+        /// </summary>
+        public void Save()
+        {
+            using (var writer = new StreamWriter(FilePath, false))
+            {
+                writer.WriteLine(Text);
+            }
+        }
+
+        /// <summary>
+        /// Прочитать данные из файла.
+        /// </summary>
+        private void ReadTextFromFile()
+        {
+            if(!File.Exists(FilePath))
+            {
+                Text = "";
+            }
+            else
+            {
+                using (var reader = new StreamReader(FilePath))
+                {
+                    Text = reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ SingletonCodeBlog/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SingletonCodeBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Проверка стандартной реализации.
+            // Можно создать несколько экземпляров класса
+            // которые будут конфликтовать между собой.
+            var text1 = new FileWorker();
+            var text2 = new FileWorker();
+
+            text1.WriteText("Hello, World!");
+            text2.WriteText("Hi, Bro!");
+
+            text1.Save();
+            text2.Save();
+
+            var singleton1 = FileWorkerSingleton.Instance;
+            var singleton2 = FileWorkerSingleton.Instance;
+
+            // var singleton = new FileWorkerSingleton(); // Создать новый экземпляр одиночки нельзя!
+
+            singleton1.WriteText("Hello, World!");
+            singleton2.WriteText("Hi, Bro!");
+
+            singleton1.Save();
+            singleton2.Save();
+        }
+    }
+}
